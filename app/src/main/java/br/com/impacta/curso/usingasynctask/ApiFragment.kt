f763d1b959760d0dfe24ea51cae52c595d89e9db
Fernando Tanaka <fernando.tomio.tanaka@gmail.com>
2023-03-01 21:33:06 -0300

    implementação ApiFragment.kt

--- app/src/main/java/br/com/impacta/curso/usingasynctask/ApiFragment.kt
+++ app/src/main/java/br/com/impacta/curso/usingasynctask/ApiFragment.kt
@@ -1,59 +1,66 @@
 package br.com.impacta.curso.usingasynctask
 
+import android.os.AsyncTask
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import br.com.impacta.curso.usingasynctask.databinding.FragmentApiBinding
+import java.net.HttpURLConnection
+import java.net.URL
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ApiFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ApiFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentApiBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_api, container, false)
+        _binding = FragmentApiBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ApiFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ApiFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.button2.setOnClickListener {
+            val cepDigitado = binding.editTextTextPersonName.text.toString()
+            val chamadaApi = ApiPostmon()
+            chamadaApi.execute(cepDigitado)
+        }
+
     }
+
+    inner class ApiPostmon: AsyncTask<String, Void, String>() {
+        override fun doInBackground(vararg p0: String?): String {
+            val cepDigitado = p0[0]!!
+            val url = URL("https://api.postmon.com.br/v1/cep/${cepDigitado}")
+
+            (url.openConnection() as? HttpURLConnection)?.let {conexao ->
+                conexao.requestMethod = "GET"
+                conexao.connect()
+
+                val inputStream = if (conexao.responseCode == HttpURLConnection.HTTP_OK) {
+                    conexao.inputStream
+                } else {
+                    conexao.errorStream
+                }
+
+                val resposta = inputStream.reader().readText()
+                return resposta
+            }
+            return "ERRO DE CASTING"
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            binding.textView2.text = result!!
+        }
+    }
+
+
 }
